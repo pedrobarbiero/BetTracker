c50168c8b8eb7866fc457ec6a2e31643c29bc507
Pedro Henrique Barbiero <pedrohenriquebarbiero@hotmail.com>
2023-11-10 02:17:27 -0400

    wip: setup application

---
+++ src/Core/Application/Persistence/Contracts/IBetRepository.cs
@@ -0,0 +1,8 @@
+﻿using Domain;
+
+namespace Application.Persistence.Contracts;
+
+public interface IBetRepository : IGenericRepository<Bet>
+{
+
+}
---
+++ src/Core/Application/Persistence/Contracts/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Common;
+
+namespace Application.Persistence.Contracts;
+
+public interface IGenericRepository<T> where T : BaseEntity
+{
+    Task<T?> GetByIdAsync(Guid id);
+    Task<T> AddAsync(T entity);
+    void Update(T entity);
+    Task<bool> DeleteAsync(Guid id);
+}
